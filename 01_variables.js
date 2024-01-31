c2c146a15f28c38342e055dd481d9a59b0348469
omkar <shelkeom230@gmail.com>
2024-01-31 18:07:51 +0530

    completed variables

---
+++ 01_variables.js
@@ -0,0 +1,23 @@
+// camecalCase is more convinient 
+
+const accountId=505050 //cannot be changed
+
+let accountEmail="shelkeom230@gmail.com"
+var accountPassword="12345"
+accountCity="Amravati"
+let accountState
+
+// accountId=2 not allowed
+
+accountEmail="cmomkar@gmail.com"
+accountPassword="omkar2005"
+accountCity="benguluru"
+console.log(accountId);
+
+// print in tabular format 
+
+/*
+Prefer not to use var
+bcz of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
