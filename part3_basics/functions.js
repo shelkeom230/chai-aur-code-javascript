0e272ec319f23b4a7acac094958533c979545ede
omkar <shelkeom230@gmail.com>
2024-02-03 16:05:29 +0530

    functions to be continued

--- part3_basics/functions.js
+++ part3_basics/functions.js
@@ -9,24 +9,22 @@ function sayMyName() {
 
 // sayMyName()
 
-function addTwoNumbers(a,b){
-    let result=a+b 
-    return result
+function addTwoNumbers(a, b) {
+  let result = a + b;
+  return result;
 }
 
-function multiply(a,b){
-    return a*b
+function multiply(a, b) {
+  return a * b;
 }
 
 // parametes: function definition
 // args: function call
 
-console.log(`addition of 2 numbers is ${addTwoNumbers(10,20)}`); //args
-console.log(`addition of 2 numbers is ${addTwoNumbers(10,5)}`);
+console.log(`addition of 2 numbers is ${addTwoNumbers(10, 20)}`); //args
+console.log(`addition of 2 numbers is ${addTwoNumbers(10, 5)}`);
 
-const result=addTwoNumbers(50,50)
-console.log("Result: ",result);
+const result = addTwoNumbers(50, 50);
+console.log("Result: ", result);
 
-console.log(`multiplication: ${multiply(10,3)}`);
-
-
+console.log(`multiplication: ${multiply(10, 3)}`);
