758ab2d7a435dc8cb2b214cf9a5a44a72c7e07dd
omkar <shelkeom230@gmail.com>
2024-02-04 19:54:19 +0530

    IIEF today work done

--- part3_basics/04_IIEF_function.js
+++ part3_basics/04_IIEF_function.js
@@ -9,3 +9,13 @@
     //unnamed IIFE
     console.log(`DB connected 2 ${name}`);
 }) ("Omkar")
+
+let val1=10
+let val2=20
+
+function addNum(num1,num2){
+    let total=num1+num2
+    return total
+}
+let result=addNum(val1,val2)
+let result2=addNum(10,2)
