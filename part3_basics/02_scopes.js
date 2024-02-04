14839927b5184815ecd9195e4413d87695ee0f07
omkar <shelkeom230@gmail.com>
2024-02-04 18:52:30 +0530

    scopes part 2

--- part3_basics/02_scopes.js
+++ part3_basics/02_scopes.js
@@ -11,3 +11,44 @@ if (true) { //block scope
 // console.log(c);
 
 
+function one(){
+    const userName="Omkar"
+
+    function two(){
+        const website="youtube"
+        // console.log(userName);
+    }
+    // console.log(website); //error due to block scope
+
+    // two()
+}
+
+// one()
+
+if(true){
+    const userName="Omkar"
+
+    if(userName==="Omkar"){
+        const website=" youtube"
+        // console.log(userName+website);
+    }
+
+    // console.log(website);
+}
+
+// console.log(userName);
+
+/***************************interesting***************** */
+
+console.log(addOne(5))
+function addOne(num){
+    return num+1
+}
+
+
+console.log(addTwo(5))
+const addTwo=function(num){ //called as expression also
+    return num+2
+}
+
+
