d9c6e6a6961e6eed32d0a9e5816f312086ce5988
omkar <shelkeom230@gmail.com>
2024-02-17 18:21:42 +0530

    eight reduce

--- part5_iterations/eight.js
+++ part5_iterations/eight.js
@@ -1,1 +1,34 @@
-// reduce 
+// reduce
+
+const myNums = [1, 2, 3];
+
+/*
+
+const myTotal=myNums.reduce(function (acc,currVal) {
+    console.log(`acc: ${acc} and currVal: ${currVal}`);
+    return acc+currVal;
+},0)
+*/
+
+const myTotal = myNums.reduce((acc, currVal) => acc + currVal, 0);
+// console.log(myTotal);
+
+const shoppingCart=[
+    {
+        itemName:"js course",
+        price:2999
+    },{
+        itemName:"python course",
+        price:999
+    },{
+        itemName:"android course",
+        price:5999
+    },{
+        itemName:"data science course",
+        price:12999
+    }
+]
+
+const priceToPay=shoppingCart.reduce((acc,item)=>acc+item.price,0)
+
+console.log(priceToPay);
