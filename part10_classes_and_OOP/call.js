43bb286b54f9d67ceb48dc4047949af8eb91c440
omkar <shelkeom230@gmail.com>
2024-02-26 18:12:31 +0530

    call in js

---
+++ part10_classes_and_OOP/call.js
@@ -0,0 +1,15 @@
+function setUsername(username) {
+  // complex DB calls
+  this.username = username;
+  console.log("called");
+}
+
+function CreateUser(username, email, password) {
+  setUsername.call(this, username);
+
+  this.email = email;
+  this.password = password;
+}
+
+const chai = new CreateUser("chai", "chai@fb.com", "123");
+console.log(chai);
