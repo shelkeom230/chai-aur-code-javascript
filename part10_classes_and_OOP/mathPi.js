9a4dce9482dc34660532ead3db00d37b38dba2ba
omkar <shelkeom230@gmail.com>
2024-02-27 19:38:10 +0530

    can we change value of PI

---
+++ part10_classes_and_OOP/mathPi.js
@@ -0,0 +1,35 @@
+const descriptor=Object.getOwnPropertyDescriptor(Math,"PI")
+
+// console.log(descriptor);
+
+
+// console.log(Math.PI);
+// Math.PI=5
+// console.log(Math.PI);
+
+// const myNewObj=Object.create(null)
+
+const chai={
+    name:'ginger chai',
+    price:250,
+    isAvailable:true ,
+
+    orderChai:function(){
+        console.log("chai nahi bani");
+    }
+}
+
+
+Object.defineProperty(chai,'name',{
+    // writable:false,
+    enumerable:true,
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+for (let [key,value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+    console.log(`${key}: ${value}`);
+    }
+}
+
