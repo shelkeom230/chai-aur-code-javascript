13c8b5a0ef30b56ecb23ed816c242f50f2fc5798
omkar <shelkeom230@gmail.com>
2024-07-08 19:17:17 +0530

    changes

--- part1_basics/08_number_maths.js
+++ part1_basics/08_number_maths.js
@@ -25,13 +25,14 @@ console.log(Math.abs(-4)); //-ve to +ve
 console.log(Math.round(4.3));
 console.log(Math.round(4.6));
 console.log(Math.ceil(4.2)); //top value
-console.log(Math.floor(4.3));
+console.log(Math.floor(4.3)); //lower int value
 console.log(Math.min(3, 4, 5, 6));
 console.log(Math.max(3, 4, 5, 6));
 
 console.log(Math.random());
 console.log(Math.floor(Math.random() * 10) + 1);
 console.log(Math.floor(Math.random()*20)+1);
+console.log(Math.floor(Math.random()*(20-10+1)+10)); //random number in between range
 
 const min = 10;
 const max = 20;
